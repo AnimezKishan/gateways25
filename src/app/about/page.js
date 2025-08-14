31bab358c31cc44f51675c4df468278d61000127
TusharGhosh56 <tusharghosh408@gmail.com>
2025-08-14 12:43:52 +0530

    about page created

---
+++ src/app/about/page.js
@@ -0,0 +1,19 @@
+'use client';
+import { LampDemo } from "@/components/about/about";
+import Navbar from "@/components/Navbar";
+
+export default function Events() {
+    return (
+        <>
+        <Navbar />
+        <main
+            style={{
+                margin: 0,
+                padding: 0,
+                width: '100vw',
+            }}>
+                <LampDemo />
+        </main>
+        </>
+    );
+}
